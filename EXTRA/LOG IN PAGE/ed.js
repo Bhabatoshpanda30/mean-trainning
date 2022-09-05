4d61c22a99e7b1d09c498ee27c6c0fdf15659268
Bhabatosh Panda <85449996+Bhabatoshpanda30@users.noreply.github.com>
2022-09-05 09:32:35 +0530

    Add files via upload

---
+++ EXTRA/LOG IN PAGE/ed.js
@@ -0,0 +1,15 @@
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const firstForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+signInBtn.addEventListener("click", ()=>
+{container.classList.remove("right-panel-active")
+});
+signUpBtn.addEventListener("click", () => {
+container.classlist.add("right-panel-active");
+});
+firstForm.addEventListener("submit", (e) =>
+e.preventDefault());
+secondForm.addEventListener("submit", (e) =>
+e.preventDefault());
